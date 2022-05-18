5a6a87500db3188e5760691e08111cd5b8396661
Tony LaForgia <alforgia428@gmail.com>
2022-05-17 22:01:46 -0500

    added attack value of player to 20

--- index.js
+++ index.js
@@ -161,6 +161,7 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false;
+    enemy.health -= 20;
     document.querySelector("#enemyHealth").style.width = enemy.health;
   }
 
