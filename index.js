ec3ec7d1ac89a4aa115465b9b2a046b017380f54
Tony LaForgia <alforgia428@gmail.com>
2022-05-19 22:10:19 -0500

    organized queries in if statements

--- index.js
+++ index.js
@@ -135,11 +135,11 @@ function decreaseTimer() {
     document.querySelector("#timer").innerHTML = timer;
   }
   if (timer === 0) {
+    document.querySelector("#displayText").style.display = "flex";
     if (player.health === enemy.health) {
       document.querySelector("#displayText").innerHTML = "Tie";
     } else if (player.health > enemy.health) {
-      document.querySelector("#displayText").innerHTML = "Player 1 Wins";
-      document.querySelector("#displayText").style.display = "flex";
+      document.querySelector("#displayText").style.display = "Player 1 Wins";
     }
   }
 }
