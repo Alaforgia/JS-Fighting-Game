735b1ae216be5ae1d74278177e2411b8f22ee1e7
Tony LaForgia <alforgia428@gmail.com>
2022-05-19 22:16:24 -0500

    added player 2 wins else if

--- index.js
+++ index.js
@@ -140,7 +140,8 @@ function decreaseTimer() {
       document.querySelector("#displayText").innerHTML = "Tie";
     } else if (player.health > enemy.health) {
       document.querySelector("#displayText").innerHTML = "Player 1 Wins";
-    }
+    } else if (player.health < enemy.health) {
+      document.querySelector("#displayText").innerHTML = "Player 2 Wins";
   }
 }
 
